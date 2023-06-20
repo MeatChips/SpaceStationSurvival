007a04f0bc0d348fb36d3e23ded2df4d635e52b5
MeatChips <84376178+MeatChips@users.noreply.github.com>
2023-06-20 15:09:51 +0200

    Menu, Settings, New scenes
    - Start Menu added
    - Settings Menu added (Full screen, resolution settings(Will probably switch to new setting system)
    - New font added
    - New scenes for the menu's
    - Updated death canvas with main menu button
    - Moved some assets to other folders

--- SpaceStationSurvival/Assets/Scripts/Menu/SceneSwitch.cs
+++ SpaceStationSurvival/Assets/Scripts/Menu/SceneSwitch.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class DeathMenu : MonoBehaviour
+public class SceneSwitch : MonoBehaviour
 {
-    public void RestartGame()
+    public void StartGame()
     {
         print("Game Restarted");
         SceneManager.LoadScene("Main");
     }
 
+    public void GoToSettings()
+    {
+        print("Go To Settings");
+        SceneManager.LoadScene("Settings");
+    }
+
     public void GoBackToMainMenu()
     {
-        print("Go To Main Menu");
+        print("Go To Main");
         SceneManager.LoadScene("MainMenu");
     }
 
---
+++ SpaceStationSurvival/Assets/Scripts/Menu/SettingsMenu.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    [SerializeField] private Toggle fullScreenToggle;
+
+    private Resolution[] resolutions;
+    private List<Resolution> filteredResolutions;
+
+    private float currentRefreshRate;
+    private int currentResolutionIndex = 0;
+
+    bool isFullscreen;
+
+    void Start()
+    {
+        fullScreenToggle.isOn = Screen.fullScreen;
+
+        resolutions = Screen.resolutions;
+        filteredResolutions = new List<Resolution>();
+
+        resolutionDropdown.ClearOptions();
+        currentRefreshRate = Screen.currentResolution.refreshRate;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            if(resolutions[i].refreshRate == currentRefreshRate)
+            {
+                filteredResolutions.Add(resolutions[i]);
+            }
+        }
+
+        List<string> options = new List<string>();
+        for (int i = 0; i < filteredResolutions.Count; i++)
+        {
+            string resolutionOption = filteredResolutions[i].width + "x" + filteredResolutions[i].height + " " + filteredResolutions[i].refreshRate + "hz";
+            options.Add(resolutionOption);
+
+            if(filteredResolutions[i].width == Screen.width && filteredResolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = Mathf.Max(options.Count);
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = Mathf.Max(options.Count);
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = filteredResolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, true);
+    }
+
+    public void SetFullScreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+        print("Changed screen mode");
+    }
+
+
+}
